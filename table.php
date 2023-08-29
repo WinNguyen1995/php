be56a8eae6849504c80893997012e63dbbc6f7aa
Win Nguyễn <74404272+WinNguyen1995@users.noreply.github.com>
2023-08-29 16:01:56 +0700

    table UI

--- table.php
+++ table.php
@@ -9,7 +9,6 @@
             font-family: arial, sans-serif;
             border-collapse: collapse;
             width: 100%;
-            align-items: center;
         }
         td, th {
             border: 1px solid #dddddd;
@@ -20,31 +19,33 @@
 </head>
 <body>
     <h2>Table & Array</h2>
-
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Address</th>
-        </tr>
-        <?php foreach ($table as $value): ?>
-        <tr>
-            <td><?= $ID->ID; ?></td>
-            <td><?= $ID->name; ?></td>
-            <td><?= $ID->age; ?></td>
-            <td><?= $ID->address; ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
     <?php
+    echo "<table>";
+    //Thêm 1 cột hành động để chứa button xóa
+    $header = array("ID", "Name", "Age", "Address", "Hành động");    
+        echo "<tr>";
+        for ($i = 0; $i <count($header); $i++) {
+            echo "<th>$header[$i]</th>";
+        }
+        echo "</tr>";
+
     $info = array
         (
-            array("Win", 27, "Hồ Chí Minh"),
-            array("Mai", 18, "Bình Dương"),
-            array("Name", 20, "Thủ Đức"),
-            array("Ngọc", 21, "Tân Uyên"),
-        );
+            array(1,"Win", 27, "Hồ Chí Minh"),
+            array(2,"Ten1", 18, "DDFG"),
+            array(3,"Ten2", 20, "TDFDF"),
+            array(4,"Ten3", 21, "AKD"),
+        );    
+        for ($row = 0; $row < count($info); $row++) {
+            echo "<tr>";
+            for ($col = 0; $col < count($info); $col++) {
+                echo "<td>" . $info[$row][$col] . "</td>";   
+            }
+            //Mỗi vòng lặp là 1 thẻ tr, trong tr có nhiều td, do những td kia cần lặp lại để hiện thị thông tin, thì sau khi lặp xong bên ngoài vòng lặp for chỉ cần thêm 1 thẻ td (vẫn phải ở trong thẻ tr) là xong
+            echo "<td><button>Xóa</button></td>";   
+            echo "</tr>";
+        }
+    echo "</table>";
     ?>
 </body>
 </html>
